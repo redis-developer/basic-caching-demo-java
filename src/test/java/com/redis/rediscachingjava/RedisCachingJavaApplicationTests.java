1af7ddb6d5d110bb0af83301e1894a9d61feb06a
oleg <gusevoleg96@gmail.ru>
2020-12-19 13:37:30 +0300

    commit

---
+++ src/test/java/com/redis/rediscachingjava/RedisCachingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.rediscachingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCachingJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
